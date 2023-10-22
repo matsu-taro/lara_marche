26a5f03f33990ea023d1c7f0f19c93ec2aaa2dc2
matsu-taro <eee.rip179@gmail.com>
2023-10-22 17:53:12 +0900

    Gaurdの設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ComponentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-1', [ComponentController::class, 'show_1']);
+Route::get('/test-2', [ComponentController::class, 'show_2']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');

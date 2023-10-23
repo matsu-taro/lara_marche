bd0384a86f7d3523628f9c43ec4c6f975d8bb5ff
matsu-taro <eee.rip179@gmail.com>
2023-10-23 15:11:59 +0900

    マルチログイン設定

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::get('/test-2', [ComponentController::class, 'show_2']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth:users', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:users')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
